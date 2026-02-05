2101f64f4cedfcfdbf38788a1fb5cffc6572200a
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2026-02-05 17:03:10 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import userRouter from './routes/userRoutes.js'
-import imageRouter from './routes/imageRoutes.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
 
-const PORT = process.env.PORT || 4000
-const app = express()
+const PORT = process.env.PORT || 4000;
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  }),
+);
 
-await connectDB()
-app.use('/api/user', userRouter)
-app.use('/api/image', imageRouter)
-app.get('/', (req, res)=>{
-    return res.send("API WORKING")
-})
+await connectDB();
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+app.get("/", (req, res) => {
+  return res.send("API WORKING");
+});
 
-app.listen(PORT, ()=>console.log("Server running on port " + PORT))
+app.listen(PORT, () => console.log("Server running on port " + PORT));
