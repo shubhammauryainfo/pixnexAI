2e47258ebd771d1ce6787381a1d7e7928b25aae7
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2026-02-05 17:16:56 +0530

    update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ import imageRouter from "./routes/imageRoutes.js";
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: ["https://pixnex.flickdesk.tech", "http://localhost:5173"],
